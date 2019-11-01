b92d16edc4f88d65462a8ce1f37cd93add12f418
Brandon <lebebr01@gmail.com>
2019-11-01 12:34:39 -0500

    Add col_names and skip

--- data-raw/baby-data.r
+++ data-raw/baby-data.r
@@ -1,5 +1,12 @@
 library(readr)
 
-baby <- read_csv("https://raw.githubusercontent.com/data-8/textbook/gh-pages/data/baby.csv")
+baby <- read_csv("https://raw.githubusercontent.com/data-8/textbook/gh-pages/data/baby.csv",
+                 col_names = c('birth_weight',
+                               'gestational_days',
+                               'maternal_age',
+                               'maternal_height',
+                               'maternal_pregnancy_weight',
+                               'maternal_smoker'),
+                 skip = 1)
 
 readr::write_csv(baby, path = "data-raw/baby.csv")
