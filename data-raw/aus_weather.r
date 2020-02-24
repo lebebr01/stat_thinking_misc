4ee7a9d559656d1d8a4af9a87628faba6c28284a
Brandon LeBeau <lebebr01@gmail.com>
2020-02-24 13:29:59 -0600

    Add man aus_weather

--- data-raw/aus_weather.r
+++ data-raw/aus_weather.r
@@ -1,4 +1,8 @@
 library(readr)
 
 aus_weather <- read_csv("data-raw/weatherAUS.csv",
-                        guess_max = 100000)
+                        guess_max = 100000) %>%
+  na.omit() %>%
+  select(-Evaporation, -Sunshine, -Cloud9am, -Cloud3pm)
+
+save(aus_weather, file = 'data/aus_weather.rda')
