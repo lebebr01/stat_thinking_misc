c3291533264acd709a4f4bac26e8f7e522418a22
Brandon <lebebr01@gmail.com>
2020-09-17 11:55:50 -0500

    Add a few other attributes

--- data-raw/june-2020-niceride.r
+++ data-raw/june-2020-niceride.r
@@ -8,8 +8,12 @@ nice_ride_2020 <- read_csv("data-raw/202006-niceride-tripdata.csv",
 # calculate trip data
 nice_ride_2020 <- nice_ride_2020 %>%
   mutate(tripduration_seconds = as.numeric(difftime(ended_at, started_at, units = 'secs')),
-         tripduration_minutes = as.numeric(difftime(ended_at, started_at, units = 'mins'))) %>%
-  select(ride_id, rideable_type, started_at, ended_at, member_casual, tripduration_seconds, tripduration_minutes) %>%
+         tripduration_minutes = as.numeric(difftime(ended_at, started_at, units = 'mins')),
+         hour = hour(started_at),
+         morning = ifelse(hour %in% 5:11, 'Morning Ride', 'Other'),
+         evening = ifelse(hour %in% 16:22, 'Evening Ride', 'Other')) %>%
+  select(ride_id, rideable_type, started_at, ended_at, member_casual, tripduration_seconds, tripduration_minutes,
+         hour, morning, evening) %>%
   filter(tripduration_minutes < 240)
 
 readr::write_csv(nice_ride_2020, path = 'data-raw/nice_ride_2020_June.csv')
